9477fa6e34bca9530d65b7e9c7e1485caaa5fd69
Oscar Centurion <centurion.oscar90@gmail.com>
2022-11-02 20:15:47 -0300

    se agregaron iconos, bootstrap y header

--- ecommercemaquillaje/src/index.js
+++ ecommercemaquillaje/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import "./node_modules/bootstrap/scss/bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
